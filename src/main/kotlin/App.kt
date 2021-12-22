19018ba4463aa610f584ba508f8eecb902c31399
Mohammad Zaki <mohdzaki407@gmail.com>
2021-12-22 12:41:35 +0530

    CLI Application Complete

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,79 @@
 import cli.App
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.default
+import cli.Filters
+import cli.Options
+import data.entity.Response
+import kotlinx.cli.*
+import utils.PasswordType
 
+@ExperimentalCli
 fun main(args: Array<String>) {
     val parser = ArgParser("${App.appName}:: ${App.version}")
-    val version by parser.option(ArgType.Boolean, shortName = "v", description = "Version").default(false)
+    val version by parser.option(
+        ArgType.Boolean,
+        shortName = Options.VERSION.option,
+        description = Options.VERSION.description
+    ).default(false)
 
+    val passwordType by parser.option(
+        ArgType.Choice<PasswordType>(),
+        shortName = Options.PASSWORD_TYPE.option,
+        description = Options.PASSWORD_TYPE.description
+    ).default(PasswordType.RANDOM)
+
+    val passwordLength by parser.option(
+        ArgType.Int,
+        shortName = Options.PASSWORD_LENGTH.option,
+        description = Options.PASSWORD_LENGTH.description
+    ).default(8)
+
+    val logs by parser.option(ArgType.Boolean, shortName = Options.LOGS.option, description = Options.LOGS.description)
+        .default(false)
+
+    val custom by parser.option(
+        ArgType.String,
+        shortName = Options.CUSTOM.option,
+        description = Options.CUSTOM.description
+    ).default("GENERATION")
+
+    val filters = ArrayList<Filters>()
+
+    class Filter : Subcommand(Options.INCLUDE.option, Options.INCLUDE.description) {
+        val numbers by argument(ArgType.String, description = Options.INCLUDE.description).vararg()
+        override fun execute() {
+            numbers.forEach {
+                when (it) {
+                    Filters.UPPERCASE.arg -> filters.add(Filters.UPPERCASE)
+                    Filters.LOWERCASE.arg -> filters.add(Filters.LOWERCASE)
+                    Filters.DIGITS.arg -> filters.add(Filters.DIGITS)
+                    Filters.SPECIAL.arg -> filters.add(Filters.SPECIAL)
+                }
+            }
+        }
+    }
+
+    parser.subcommands(Filter())
     parser.parse(args)
 
-    if(version) println(App.version)
+    if (version) println(App.version)
+    else {
+        PasswordGenerator.Builder(passwordType)
+            .showLogs(logs)
+            .includeUpperCaseChars(filters.contains(Filters.UPPERCASE))
+            .includeNumbers(filters.contains(Filters.DIGITS))
+            .includeLowerCaseChars(filters.contains(Filters.LOWERCASE))
+            .includeSpecialSymbols(filters.contains(Filters.SPECIAL))
+            .customPassword(custom)
+            .passwordLength(passwordLength)
+            .callback(object : PasswordGenerator.Callback {
+                override fun onPasswordGenerated(response: Response) {
+                    println("\n${App.appName} (${App.version})")
+                    println(response.password)
+                    println("Password Generated Successfully!\n")
+                }
+            })
+            .build()
+            .generate()
+    }
+
+
 }
--- src/main/kotlin/cli/Constants.kt
+++ src/main/kotlin/cli/Constants.kt
@@ -4,3 +4,20 @@ object App {
     const val appName = "Password Generator CLI"
     const val version = "0.1"
 }
+
+enum class Options(val option: String, val description: String) {
+    VERSION("version","Shows Version"),
+    PASSWORD_TYPE("type","Password Type"),
+    PASSWORD_LENGTH("length","Set Password Length"),
+    LOGS("showLogs","Logs"),
+    CUSTOM("custom","Custom Password"),
+    INCLUDE("include","Include Filters( upper lower digits special )d"),
+    ;
+}
+
+enum class Filters(val arg: String) {
+    UPPERCASE("upper"),
+    LOWERCASE("lower"),
+    SPECIAL("special"),
+    DIGITS("digits")
+}
